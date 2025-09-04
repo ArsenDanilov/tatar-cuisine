33618769115c9f96b35942f4161508b9cc89f480
Arsen.Danilov <arsen.danilov@ite.group>
2025-09-04 14:26:33 +0300

    Set up the project and create menu

---
+++ src/app/ingredients/page.tsx
@@ -0,0 +1,7 @@
+const IngredientsPage = () => {
+    return (
+        <h1>Ингредиенты</h1>
+    )
+}
+
+export default IngredientsPage;
---
+++ src/components/UI/header.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { siteConfig } from "@/config/site.config";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Button,
+} from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const Logo = () => {
+  return (
+    <Image
+      src="/logo_tatar_kitchen.png"
+      alt={siteConfig.title}
+      width={26}
+      height={26}
+      priority
+    />
+  );
+};
+
+export default function Header() {
+  const pathName = usePathname();
+
+  const getNavItems = () => {
+    return siteConfig.navItems.map((item) => {
+      const isActive = pathName === item.href;
+      return (
+        <NavbarItem key={item.href}>
+          <Link
+            color="foreground"
+            href={item.href}
+            className={`px-3 py-1 
+              ${isActive ? "text-blue-500" : "text-foreground"} 
+              hover:text-blue-300 hover:outline
+              hover:outline-blue-300 hover:rounded-md
+              transition-colors
+              transition-outline
+              duration-200`}
+          >
+            {item.label}
+          </Link>
+        </NavbarItem>
+      );
+    });
+  };
+
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <Link href="/" className="flex gap-1">
+          <Logo />
+        </Link>
+        <p className="font-bold text-inherit">{siteConfig.title}</p>
+      </NavbarBrand>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        {getNavItems()}
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Логин</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Регистрация
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      
+    </Navbar>
+  );
+}
